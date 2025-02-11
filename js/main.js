bfc758426a26ea01cb319a181306220450c48b95
Piotr <piotrekrucki17@gmail.com>
2025-02-11 12:13:39 +0100

    added option to display video as page background

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar');
-	console.log(nav);
 	function addShadow() {
 		if (window.scrollY >= 100) {
 			nav.classList.add('shadow-bg');
@@ -10,4 +9,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	window.addEventListener('scroll', addShadow);
+});
+
+const homeHeader = document.querySelector('#home');
+const video = document.querySelector('.video');
+const toggleBackgroundBtn = document.querySelector('.toggle-background');
+
+toggleBackgroundBtn.addEventListener('click', () => {
+	if (homeHeader.classList.contains('hero-video')) {
+		homeHeader.classList.remove('hero-video');
+		video.classList.add('d-none');
+		homeHeader.classList.add('hero-img');
+		toggleBackgroundBtn.textContent = 'Zmień tło na wideo';
+	} else if (homeHeader.classList.contains('hero-img')) {
+		homeHeader.classList.remove('hero-img');
+		video.classList.remove('d-none');
+		homeHeader.classList.add('hero-video');
+		toggleBackgroundBtn.textContent = 'Zmień tło na obraz';
+	}
 });
