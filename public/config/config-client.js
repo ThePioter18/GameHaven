9aa25fee2107d486840390353f41fd46f413e9db
Piotr <piotrekrucki17@gmail.com>
2025-10-09 18:56:36 +0200

    fixed config-client.js

--- public/config/config-client.js
+++ public/config/config-client.js
@@ -1,12 +1,7 @@
-const isNetlify = window.location.hostname.includes('netlify.app');
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 const CONFIG = {
-	baseURL: isNetlify
-		? '/.netlify/functions' // Netlify Functions
-		: isLocalhost
-		? 'http://localhost:5000' // local Express backend
-		: window.CONFIG.baseURL, // production Render from file generate-env.js
+	baseURL: isLocalhost ? 'http://localhost:5000' : window.CONFIG.baseURL,
 };
 
 export default CONFIG;
